2d4ce65d890511cece7854bf71a87bc65523d297
Reinaldo Gonzalez <reinaldoagf1@gmail.com>
2022-04-03 23:58:24 -0400

    Actualizacion de package.json

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
-app.use(express.static(path.join(__dirname, './dist/instaredux')));
 
-app.get('/*',(req, res)=>{
-	res.sendFile(path.join(__dirname, '/dist/instaredux/index.html'))
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/instaredux'));
 
-app.listen(process.env.PORT||8080,()=>{
-	console.log("Server started")
-})
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/instaredux/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
